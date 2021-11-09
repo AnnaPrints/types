67446c57afd79528a4a4d110aba3ac984d66fdba
AnnaPrints <65970510+AnnaPrints@users.noreply.github.com>
2021-11-09 16:58:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+    llistн = []
+    llist.append(1)
+
+
+    print("hello world")
+a = 1
+b = 2
+c = 6
+print(a + b)
+if __name__ == '__main__': print("main")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#
+#
+
+#
+#
+#
+
+#
+#
+
+#
+
+
+#
